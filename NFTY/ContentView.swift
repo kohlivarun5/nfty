1553571358a33de7906317f3df14d27056a9b49d
Varun Kohli <kohli.varun5@gmail.com>
2021-04-17 10:31:03 -0400

    Initial Commit

---
+++ NFTY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFTY
+//
+//  Created by Varun Kohli on 4/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NFTY/NFTYApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTYApp.swift
+//  NFTY
+//
+//  Created by Varun Kohli on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct NFTYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
