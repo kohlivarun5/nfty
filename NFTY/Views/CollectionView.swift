aabf6ee5c595b1b1dc7203cf6caa200c976ce631
Varun Kohli <kohli.varun5@gmail.com>
2021-04-21 00:26:23 -0400

    pinned

--- NFTY/Views/CollectionView.swift
+++ NFTY/Views/CollectionView.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
+struct VisualEffectView: UIViewRepresentable {
+  var effect: UIVisualEffect?
+  func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+  func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+}
+
 struct CollectionView: View {
   
   var collection : CollectionInfo
   
   @State private var showSorted = false
   @State private var filterZeros = false
+  @State private var selectedNumber = 0
+  
+  @State private var action: String? = ""
+
   
   func sorted(l:[NFT]) -> [NFT] {
     showSorted ? l.sorted(by:{$0.eth < $1.eth}) : l
@@ -33,22 +43,49 @@ struct CollectionView: View {
   
   var body: some View {
    
-    List {
-      Toggle(isOn: $showSorted) {
-        Text("Sort Low to High")
-      }
-      Toggle(isOn: $filterZeros) {
-        Text("Filter Zero")
-      }
-      ForEach(
-        sorted(l:filtered(l:collection.nfts)),id:\.tokenId) { nft in
-        let samples = [collection.url1,collection.url2,collection.url3,collection.url4];
-        ZStack {
-          RoundedImage(nft:nft,samples:samples,themeColor:collection.themeColor)
-            .padding()
-          NavigationLink(destination: NftDetail(nft:nft,samples:samples,themeColor:collection.themeColor)) {}
-            .hidden()
-        }
+    ScrollView {
+      LazyVStack(pinnedViews:[.sectionHeaders]){
+        Section(header:
+                  ZStack {
+                    
+                    VisualEffectView(effect: UIBlurEffect(style: .dark))
+                      .edgesIgnoringSafeArea(.all)
+                    
+                    VStack {
+                      Picker(selection: $selectedNumber, label: EmptyView()) {
+                        Text("Recent").tag(0)
+                        Text("Top").tag(1)
+                      }
+                      .pickerStyle(SegmentedPickerStyle())
+                      Toggle(isOn: $showSorted) {
+                        Text("Sort Low to High")
+                      }
+                      Toggle(isOn: $filterZeros) {
+                        Text("Filter Zero")
+                      }
+                    }
+                    .padding()
+                    
+                  }
+        ) {
+          
+          
+          ForEach(
+            sorted(l:filtered(l:collection.nfts)),id:\.tokenId) { nft in
+            let samples = [collection.url1,collection.url2,collection.url3,collection.url4];
+            ZStack {
+              RoundedImage(nft:nft,samples:samples,themeColor:collection.themeColor)
+                .padding()
+                .onTapGesture {
+                  //perform some tasks if needed before opening Destination view
+                  self.action = nft.tokenId
+                }
+              
+              NavigationLink(destination: NftDetail(nft:nft,samples:samples,themeColor:collection.themeColor),tag:nft.tokenId,selection:$action) {}
+                .hidden()
+            }
+          }
+        }.textCase(nil)
       }
     }
     .navigationBarTitle(collection.name)
