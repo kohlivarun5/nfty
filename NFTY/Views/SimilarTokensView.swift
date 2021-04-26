e961543935f4f15c11a857098d62b53de6f65026
Varun Kohli <kohli.varun5@gmail.com>
2021-04-26 19:40:16 -0400

    .

--- NFTY/Views/SimilarTokensView.swift
+++ NFTY/Views/SimilarTokensView.swift
@@ -21,24 +21,28 @@ struct SimilarTokensView: View {
     VStack {
       Divider()
       //Text("Similar Tokens").padding()
-      ScrollView(.horizontal) {
-        LazyHStack {
-          ForEach(nfts.indices,id: \.self) { index in
-            let nft = nfts[index];
-            let samples = [info.url1,info.url2,info.url3,info.url4];
-            ZStack {
-              RoundedImage(nft:nft,samples:samples,themeColor:info.themeColor,width: .narrow)
-                .scaleEffect(0.9)
-                .onTapGesture {
-                  //perform some tasks if needed before opening Destination view
-                  self.action = String(nft.tokenId)
-                }
-              
-              NavigationLink(destination: NftDetail(nft:nft,samples:samples,themeColor:info.themeColor),tag:String(nft.tokenId),selection:$action) {}
-                .hidden()
+      HStack {
+        Spacer()
+        ScrollView(.horizontal) {
+          LazyHStack {
+            ForEach(nfts.indices,id: \.self) { index in
+              let nft = nfts[index];
+              let samples = [info.url1,info.url2,info.url3,info.url4];
+              ZStack {
+                RoundedImage(nft:nft,samples:samples,themeColor:info.themeColor,width: .narrow)
+                  .scaleEffect(0.9)
+                  .onTapGesture {
+                    //perform some tasks if needed before opening Destination view
+                    self.action = String(nft.tokenId)
+                  }
+                
+                NavigationLink(destination: NftDetail(nft:nft,samples:samples,themeColor:info.themeColor,similarTokens:info.similarTokens(nft.tokenId)),tag:String(nft.tokenId),selection:$action) {}
+                  .hidden()
+              }
             }
           }
         }
+        Spacer()
       }
     }.onAppear {
       tokens.forEach { tokenId in
