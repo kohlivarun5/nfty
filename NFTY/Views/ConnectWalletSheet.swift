ac1351e3d7ec8730bfdc28c19ed30b0fc23701e4
Varun Kohli <kohli.varun5@gmail.com>
2021-06-13 10:26:49 -0400

    .

--- NFTY/Views/ConnectWalletSheet.swift
+++ NFTY/Views/ConnectWalletSheet.swift
@@ -11,144 +11,22 @@ import Web3
 struct ConnectWalletSheet: View {
   
   @Environment(\.presentationMode) var presentationMode
-  
+   
   @Binding var address : EthereumAddress?
   @State var badAddressError : String = ""
-  
-  @State var badImportWalletError : String = ""
   
   var body: some View {
     VStack {
       Spacer()
       
       VStack {
-        
-        
-        
-        Button(action: {
-        }) {
-          
-          HStack(spacing:50) {
-            Spacer()
-            VStack {
-              Image(systemName: "at.badge.plus")
-                .font(.title)
-              Text("Create New Wallet")
-                .font(.title3)
-                .fontWeight(.bold)
-            }
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .padding()
-            .foregroundColor(.white)
-            .background(Color.green)
-            .cornerRadius(40)
-            
-            Spacer()
-          }
-        }
-      }
-      .padding()
-      .animation(.easeIn)
-      
-      ZStack {
-        Divider()
-        Text("OR")
-          .font(.caption).italic()
-          .foregroundColor(.secondaryLabel)
-          .padding(.trailing)
-          .padding(.leading)
-          .background(Color.systemBackground)
-      }
-      
-      VStack {
-        
-        
-        Text("Import Wallet")
+        Text("Add Wallet using Address")
           .font(.title2)
           .fontWeight(.bold)
         
         Text("")
           .font(.title2)
-        
-        HStack(spacing:30) {
-          
-          Spacer()
-          Button(action: {
-            
-            if let string = UIPasteboard.general.string {
-              // text was found and placed in the "string" constant
-              print(string)
-            }
-            
-          }) {
-            
-            VStack {
-              Image(systemName: "doc.on.clipboard")
-                .font(.title)
-              Text("Paste seed phrase")
-                .font(.caption)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-            }
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .padding()
-            .foregroundColor(.white)
-            .background(Color.orange)
-            .cornerRadius(40)
-          }
-          
-          Button(action: {
-            if let string = UIPasteboard.general.string {
-              // text was found and placed in the "string" constant
-              print(string)
-            }
-          }) {
-            
-            
-            VStack {
-              Image(systemName: "doc.on.clipboard")
-                .font(.title)
-              Text("Paste privatekey")
-                .font(.caption)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-            }
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .padding()
-            .foregroundColor(.white)
-            .background(Color.orange)
-            .cornerRadius(40)
-            
-          }
-          Spacer()
-        }
-        
-        
-        Text(badImportWalletError)
-          .font(.footnote)
-          .foregroundColor(.secondary)
-      }
-      .padding()
-      .animation(.easeIn)
-      
-      ZStack {
-        Divider()
-        Text("OR")
-          .font(.caption).italic()
-          .foregroundColor(.secondaryLabel)
-          .padding(.trailing)
-          .padding(.leading)
-          .background(Color.systemBackground)
-      }
-      
-      VStack {
-        Text("Save Wallet Address")
-          .font(.title2)
-          .fontWeight(.bold)
-        
-        Text("")
-          .font(.title2)
-        
+
         Button(action: {
           if let string = UIPasteboard.general.string {
             // text was found and placed in the "string" constant
@@ -176,7 +54,7 @@ struct ConnectWalletSheet: View {
             .frame(minWidth: 0, maxWidth: .infinity)
             .padding()
             .foregroundColor(.white)
-            .background(Color.gray)
+            .background(Color.green)
             .cornerRadius(40)
             
             Spacer()
