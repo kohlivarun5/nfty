62117eda532cd319551400bfbbea614005542f86
Varun Kohli <kohli.varun5@gmail.com>
2021-06-24 20:38:21 -0400

    .

---
+++ NFTY/Model/CoreCollections.swift
@@ -0,0 +1,232 @@
+//
+//  CoreCollections.swift
+//  NFTY
+//
+//  Created by Varun Kohli on 6/22/21.
+//
+
+import Foundation
+
+let CompositeCollection = CompositeRecentTradesObject([
+  CompositeRecentTradesObject.CollectionInitializer(
+    info:CollectionInfo(
+      address:cryptoPunksContract.contractAddressHex,
+      url1:SAMPLE_PUNKS[0],
+      url2:SAMPLE_PUNKS[1],
+      url3:SAMPLE_PUNKS[2],
+      url4:SAMPLE_PUNKS[3],
+      name:"CryptoPunks",
+      webLink: URL(string:"https://www.larvalabs.com/cryptopunks")!,
+      themeColor:Color.yellow,
+      themeLabelColor:Color.systemBackground,
+      subThemeColor: /* FFB61E */ Color(red: 255/255, green: 182/255, blue: 30/255),
+      collectionColor:Color.yellow,
+      disableRecentTrades:false,
+      blur:0,
+      samplePadding:10,
+      similarTokens : { tokenId in CryptoPunks_nearestTokens[safe:Int(tokenId)] },
+      rarityRank : { tokenId in CryptoPunks_rarityRanks[safe:Int(tokenId)] }),
+    contract:cryptoPunksContract),
+  CompositeRecentTradesObject.CollectionInitializer(
+    info:CollectionInfo(
+      address:autoGlyphsContract.contractAddressHex,
+      url1:SAMPLE_AUTOGLYPHS[0],
+      url2:SAMPLE_AUTOGLYPHS[1],
+      url3:SAMPLE_AUTOGLYPHS[2],
+      url4:SAMPLE_AUTOGLYPHS[3],
+      name:"Autoglyphs",
+      webLink: URL(string:"https://www.larvalabs.com/autoglyphs")!,
+      themeColor:Color.label,
+      themeLabelColor:Color.gray,
+      subThemeColor:Color.label,
+      collectionColor:Color.white,
+      disableRecentTrades:false,
+      blur:0,
+      samplePadding:10,
+      similarTokens: { tokenId in nil },
+      rarityRank : { tokenId in nil }),
+    contract:autoGlyphsContract),
+  CompositeRecentTradesObject.CollectionInitializer(
+    info:CollectionInfo(
+      address:asciiPunksContract.contractAddressHex,
+      url1:SAMPLE_ASCII_PUNKS[0],
+      url2:SAMPLE_ASCII_PUNKS[1],
+      url3:SAMPLE_ASCII_PUNKS[2],
+      url4:SAMPLE_ASCII_PUNKS[3],
+      name:"AsciiPunks",
+      webLink: URL(string:"https://asciipunks.com")!,
+      themeColor:Color.label,
+      themeLabelColor:Color.systemBackground,
+      subThemeColor:Color.label,
+      collectionColor:Color.black,
+      disableRecentTrades:false,
+      blur:0,
+      samplePadding:10,
+      similarTokens : { tokenId in AsciiPunks_nearestTokens[safe:Int(tokenId)] },
+      rarityRank : { tokenId in AsciiPunks_rarityRanks[safe:Int(tokenId)] }),
+    contract:asciiPunksContract),
+  CompositeRecentTradesObject.CollectionInitializer(
+    info:CollectionInfo(
+      address:baycContract.contractAddressHex,
+      url1:SAMPLE_BAYC[0],
+      url2:SAMPLE_BAYC[1],
+      url3:SAMPLE_BAYC[2],
+      url4:SAMPLE_BAYC[3],
+      name:"BoredApeYachtClub",
+      webLink: URL(string:"https://boredapeyachtclub.com/#/")!,
+      themeColor:Color.black,
+      themeLabelColor:Color.white,
+      subThemeColor:Color.white,
+      collectionColor:Color.black,
+      disableRecentTrades:false,
+      blur:0,
+      samplePadding:15,
+      similarTokens: { tokenId in nil },
+      rarityRank : { tokenId in nil }),
+    contract:baycContract),
+  CompositeRecentTradesObject.CollectionInitializer(
+    info:CollectionInfo(
+      address:cryptoKittiesContract.contractAddressHex,
+      url1:SAMPLE_KITTIES[0],
+      url2:SAMPLE_KITTIES[1],
+      url3:SAMPLE_KITTIES[2],
+      url4:SAMPLE_KITTIES[3],
+      name:"CryptoKitties",
+      webLink: URL(string:"https://www.cryptokitties.co")!,
+      themeColor: /* 78e08f */ Color(red: 120/255, green: 224/255, blue: 143/255),
+      themeLabelColor:Color.systemBackground,
+      subThemeColor: /* 78e08f */ Color(red: 120/255, green: 224/255, blue: 143/255),
+      collectionColor:/* 78e08f */ Color(red: 120/255, green: 224/255, blue: 143/255),
+      disableRecentTrades:true,
+      blur:0,samplePadding:0,
+      similarTokens: { tokenId in nil },
+      rarityRank : { tokenId in nil }),
+    contract:cryptoKittiesContract),
+]
+)
+
+let SampleToken = NFT(
+  address: "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+  tokenId: 340, name: "CryptoPunks",
+  media: .image(MediaImageEager(URL(string:"https://www.larvalabs.com/public/images/cryptopunks/punk0385.png")!)))
+
+let SampleCollection = CompositeCollection.collections[0]
+
+let COLLECTIONS : [Collection] = CompositeCollection.collections
+
+struct CollectionsFactory {
+  
+  let collections : [String : Collection] = Dictionary(uniqueKeysWithValues: COLLECTIONS.map{ ($0.info.address,$0) })
+  
+  func getByAddress(_ address:String) -> Collection? {
+    return collections[address]
+  }
+  
+}
+
+let collectionsFactory = CollectionsFactory()
+
+extension Array {
+  subscript (safe index: Int) -> Element? {
+    return indices ~= index ? self[index] : nil
+  }
+}
+
+extension String {
+  /*
+   Truncates the string to the specified length number of characters and appends an optional trailing string if longer.
+   - Parameter length: Desired maximum lengths of a string
+   - Parameter trailing: A 'String' that will be appended after the truncation.
+   
+   - Returns: 'String' object.
+   */
+  func trunc(length: Int, trailing: String = "…") -> String {
+    return (self.count > length) ? self.prefix(length) + trailing : self
+  }
+  
+  func deletingPrefix(_ prefix: String) -> String {
+    guard self.hasPrefix(prefix) else { return self }
+    return String(self.dropFirst(prefix.count))
+  }
+}
+
+extension URL {
+  func params() -> [String:Any] {
+    var dict = [String:Any]()
+    
+    if let components = URLComponents(url: self, resolvingAgainstBaseURL: false) {
+      if let queryItems = components.queryItems {
+        for item in queryItems {
+          dict[item.name] = item.value!
+        }
+      }
+      return dict
+    } else {
+      return [:]
+    }
+  }
+}
+
+let SAMPLE_WALLET_ADDRESS = try! EthereumAddress(
+  hex: "0x208b82b04449cd51803fae4b1561450ba13d9510",
+  eip55:false)
+
+enum CloudDefaultStorageKeys : String {
+  case walletAddress = "walletAddress"
+  case favoritesDict = "favoritesDict"
+  case friendsDict = "friendsDict"
+}
+
+class NftOwnerTokens : ObservableObject {
+  @Published var tokens: [NFTWithLazyPrice] = []
+  
+  enum LoadingState {
+    case notLoaded
+    case loading
+    case loaded
+  }
+  @Published var state : LoadingState = .notLoaded
+  
+  let ownerAddress : EthereumAddress
+  private let contracts : [ContractInterface]
+  
+  private var pendingCount = 0
+  
+  init(ownerAddress:EthereumAddress) {
+    self.ownerAddress = ownerAddress
+    self.contracts = COLLECTIONS.map { $0.data.contract }
+  }
+  
+  func load() {
+    if (state != .notLoaded) { return }
+    
+    state = .loading
+    contracts.forEach { contract in
+      contract.getOwnerTokens(
+        address:ownerAddress,
+        
+        onDone: {
+          DispatchQueue.main.async {
+            self.state = .loaded
+          }
+        }
+      ) { token in
+        DispatchQueue.main.async {
+          self.tokens.append(token)
+        }
+      }
+    }
+  }
+  
+}
+
+var OwnerTokensCache : [EthereumAddress:NftOwnerTokens] = [:]
+func getOwnerTokens(_ address:EthereumAddress) -> NftOwnerTokens {
+  switch OwnerTokensCache[address] {
+  case .some(let tokens):
+    return tokens
+  case .none:
+    OwnerTokensCache[address] = NftOwnerTokens(ownerAddress: address)
+    return OwnerTokensCache[address]!
+  }
+}
---
+++ NFTY/Views/UIExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIExtensions.swift
+//  NFTY
+//
+//  Created by Varun Kohli on 6/22/21.
+//
+
+import Foundation
+
+public extension Color {
+  static let lightText = Color(UIColor.lightText)
+  static let darkText = Color(UIColor.darkText)
+  
+  static let label = Color(UIColor.label)
+  static let secondaryLabel = Color(UIColor.secondaryLabel)
+  static let tertiaryLabel = Color(UIColor.tertiaryLabel)
+  static let quaternaryLabel = Color(UIColor.quaternaryLabel)
+  
+  static let systemBackground = Color(UIColor.systemBackground)
+  static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
+  static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
+  
+  // There are more..
+}
