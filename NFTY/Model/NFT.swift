2ed140b19db99caf5398b30d8a465a37ee9ca8e6
Varun Kohli <kohli.varun5@gmail.com>
2021-08-08 21:50:26 -0400

    .

--- NFTY/Model/NFT.swift
+++ NFTY/Model/NFT.swift
@@ -336,5 +336,8 @@ let FLS_rarityRanks : [UInt] = load("FameLadySquad_rarityRanks.json")
 let CRHDL_nearestTokens : [[UInt]] = load("CryptoHodlers_nearestTokens.json")
 let CRHDL_rarityRanks : [UInt] = load("CryptoHodlers_rarityRanks.json")
 
+let CCD_nearestTokens : [[UInt]] = load("CryptoCannabisClub_nearestTokens.json")
+let CCD_rarityRanks : [UInt] = load("CryptoCannabisClub_rarityRanks.json")
+
 let CypherCity_nearestTokens : [[UInt]] = load("CypherCity_nearestTokens.json")
 let CypherCity_rarityRanks : [UInt] = load("CypherCity_rarityRanks.json")
