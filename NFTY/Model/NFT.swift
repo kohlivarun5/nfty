a3cb01be8b3e873066b208ec3c8652ad5bd64199
Varun Kohli <kohli.varun5@gmail.com>
2021-08-14 03:13:51 -0400

    .

--- NFTY/Model/NFT.swift
+++ NFTY/Model/NFT.swift
@@ -342,14 +342,6 @@ let SAMPLE_DEAD_FELLAZ : [String] = [
   "DEAD_FELLAZ4"
 ]
 
-let SAMPLE_GCG : [String] = [
-  "GUTTER_CAT1",
-  "GUTTER_CAT2",
-  "GUTTER_CAT3",
-  "GUTTER_CAT4"
-]
-
-
 let CryptoPunks_nearestTokens : [[UInt]] = load("CryptoPunks_nearestTokens.json")
 let CryptoPunks_rarityRanks : [UInt] = load("CryptoPunks_rarityRanks.json")
 
