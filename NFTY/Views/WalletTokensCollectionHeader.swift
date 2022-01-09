52b57624ae14f0259aa73b773d00cf21b0d9bc12
Varun Kohli <kohli.varun5@gmail.com>
2022-01-09 16:03:04 -0500

    .

--- NFTY/Views/WalletTokensCollectionHeader.swift
+++ NFTY/Views/WalletTokensCollectionHeader.swift
@@ -9,23 +9,34 @@ import SwiftUI
 
 struct WalletTokensCollectionHeader: View {
   let collection : Collection
-    var body: some View {
-      HStack {
-        Spacer()
-        VStack(spacing:0) {
+  var body: some View {
+    HStack {
+      Spacer()
+      ObservedPromiseView(
+        data:ObservablePromise(promise: collection.data.contract.indicativeFloor()),
+        progress: {
           Text(collection.info.name)
-            .font(.subheadline)
-        }
+        },
+        view: { floor in
+          switch(floor) {
+          case .none:
+            Text(collection.info.name)
+          case .some(let floor):
+            VStack(spacing:0) {
+              Text(collection.info.name)
+              Text("Floor \(ethFormatter.string(for:floor)!)")
+                .font(.footnote)
+                .foregroundColor(.secondaryLabel)
+            }
+          }
+        })
         .padding(5)
-        Spacer()
-      }
-      .foregroundColor(.accentColor)
-      .background(Color.tertiarySystemBackground)
-      /* .overlay(
-        Capsule()
-          .stroke(Color.accentColor, lineWidth: 4)
-      ) */
-      .clipShape(Capsule())
-      .padding([.leading,.trailing],30)
+      Spacer()
     }
+    .foregroundColor(.accentColor)
+    .background(Color.systemBackground.opacity(0.9))
+    .clipShape(Capsule())
+    .shadow(color:.accentColor,radius:1)
+    .padding([.leading,.trailing],20)
+  }
 }
