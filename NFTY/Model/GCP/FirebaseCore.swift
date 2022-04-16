b7e60fb2913d8b370bb5d7911b2f38b5b791492f
Varun Kohli <kohli.varun5@gmail.com>
2022-04-16 05:11:42 -0400

    .

--- NFTY/Model/GCP/FirebaseCore.swift
+++ NFTY/Model/GCP/FirebaseCore.swift
@@ -20,6 +20,7 @@ struct FirebaseCore {
   func putObject(path:String, _ data:Data) -> Promise<Void>{
     return Promise { seal in
       // Upload the file to the path "images/rivers.jpg"
+      print("Calling firebase for PUT:\(path)")
       getStorage().reference(withPath:path).putData(data, metadata: nil) { (metadata, error) in
         seal.fulfill(())
       }.resume()
@@ -28,6 +29,7 @@ struct FirebaseCore {
   
   func getObject(path:String) -> Promise<Data?> {
     return Promise { seal in
+      print("Calling firebase for GET:\(path)")
       getStorage().reference(withPath:path).getData(maxSize:maxSize) { data, error in
         if let _ = error {
           // print(error)
