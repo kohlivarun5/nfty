130a2e819887065c4136e9ab43dfca4493026d5e
Varun Kohli <kohli.varun5@gmail.com>
2022-05-27 19:47:31 -0400

    .

--- NFTY/Views/PrivateCollectionView.swift
+++ NFTY/Views/PrivateCollectionView.swift
@@ -10,11 +10,6 @@ import Web3
 
 struct PrivateCollectionView: View {
   
-  @State private var friendName : String? = nil
-  @State private var fallbackName : String? = nil
-  
-  @State private var showDialog = false
-  
   enum TokensPage : Int {
     case bought
     case sales
@@ -25,43 +20,6 @@ struct PrivateCollectionView: View {
   
   let account : UserAccount
   
-  private func key() -> String? {
-    switch(account.ethAddress,account.nearAccount) {
-    case (.some(let address),_):
-      return address.hex(eip55: true)
-    case (_,.some(let account)):
-      return account
-    case (.none,.none):
-      return nil
-    }
-  }
-  
-  private func setFriend(_ name:String?) {
-    
-    guard let key = key() else { return }
-    
-    self.friendName = name
-    switch (NSUbiquitousKeyValueStore.default.object(forKey: CloudDefaultStorageKeys.friendsDict.rawValue) as? [String : String]) {
-    case .none:
-      switch name {
-      case .some(let name):
-        var friends : [String : String] = [:]
-        friends[key] = name
-        NSUbiquitousKeyValueStore.default.set(friends,forKey:CloudDefaultStorageKeys.friendsDict.rawValue)
-      case .none:
-        break
-      }
-    case .some(var friends):
-      switch name {
-      case .some(let name):
-        friends[key] = name
-      case .none:
-        friends.removeValue(forKey: key)
-      }
-      NSUbiquitousKeyValueStore.default.set(friends,forKey:CloudDefaultStorageKeys.friendsDict.rawValue)
-    }
-  }
-  
   let nft = SampleToken
   let collection = SampleCollection
   
@@ -70,21 +28,10 @@ struct PrivateCollectionView: View {
     
     VStack(spacing:0) {
       
-      ProfileViewHeader(name:friendName,account:account)
+      ProfileViewHeader(account:account)
         .padding(.top,50)
         .padding(.bottom,10)
         .background(Color.secondarySystemBackground)
-        .onAppear {
-          guard let key = key() else { return }
-          let friends = NSUbiquitousKeyValueStore.default.object(forKey: CloudDefaultStorageKeys.friendsDict.rawValue) as? [String : String]
-          self.friendName = friends?[key]
-          self.fallbackName = friendName ?? account.nearAccount
-          
-          guard let address = self.account.ethAddress else { return }
-          ENSContract.nameOfOwner(address, eth: web3.eth)
-            .done(on:.main) { $0.map { self.friendName = $0 } }
-            .catch { print($0) }
-        }
       
       VStack(spacing:0) {
         
@@ -127,18 +74,5 @@ struct PrivateCollectionView: View {
     }
     .navigationBarTitle("",displayMode:.large)
     .ignoresSafeArea(edges: .top)
-    .navigationBarItems(
-      trailing: Button(action: {
-        switch (self.friendName) {
-        case .none:
-          self.showDialog = true
-        case .some:
-          self.setFriend(nil)
-        }
-      }, label: {
-        Image(systemName: friendName == .none ? "person.crop.circle.badge.plus" : "person.crop.circle.badge.minus")
-          .renderingMode(.original)
-      })
-    )
   }
 }
--- NFTY/Views/ProfileViewHeader.swift
+++ NFTY/Views/ProfileViewHeader.swift
@@ -10,10 +10,51 @@ import Web3
 
 struct ProfileViewHeader: View {
   
-  let name : String?
   let account : UserAccount
   
   @State private var balance : EthereumQuantity? = nil
+  
+  @State private var isFollowing = false
+  
+  @State private var friendName : String?
+  @State private var showDialog = false
+  
+  private func setFriend(_ name:String) {
+    
+    guard let key = key() else { return }
+    
+    self.isFollowing = !self.isFollowing
+    switch (NSUbiquitousKeyValueStore.default.object(forKey: CloudDefaultStorageKeys.friendsDict.rawValue) as? [String : String]) {
+    case .none:
+      switch self.isFollowing {
+      case true:
+        var friends : [String : String] = [:]
+        friends[key] = name
+        NSUbiquitousKeyValueStore.default.set(friends,forKey:CloudDefaultStorageKeys.friendsDict.rawValue)
+      case false:
+        break
+      }
+    case .some(var friends):
+      switch self.isFollowing {
+      case true:
+        friends[key] = name
+      case false:
+        friends.removeValue(forKey: key)
+      }
+      NSUbiquitousKeyValueStore.default.set(friends,forKey:CloudDefaultStorageKeys.friendsDict.rawValue)
+    }
+  }
+  
+  private func key() -> String? {
+    switch(account.ethAddress,account.nearAccount) {
+    case (.some(let address),_):
+      return address.hex(eip55: true)
+    case (_,.some(let account)):
+      return account
+    case (.none,.none):
+      return nil
+    }
+  }
   
   var body: some View {
     
@@ -34,7 +75,7 @@ struct ProfileViewHeader: View {
       .shadow(color:.accentColor,radius:0)
       
       VStack(alignment:.leading,spacing:5) {
-        name.map { name in
+        friendName.map { name in
           HStack {
             Text(name)
               .font(.headline)
@@ -49,16 +90,63 @@ struct ProfileViewHeader: View {
             Spacer()
           }
         }
+        
+        friendName.map { name in
+          Button(action: {
+            setFriend(name)
+          }) {
+            HStack {
+              Spacer()
+              Text(isFollowing ? "Unfollow" : "Follow")
+                .font(.caption).bold()
+              Spacer()
+            }
+          }
+          .padding([.top,.bottom],5)
+          .padding([.leading,.trailing])
+          .background(.ultraThinMaterial, in: Capsule())
+          .padding(.top)
+          .padding(.trailing)
+          .foregroundColor(isFollowing ? .accentColor : .label)
+          .if(!isFollowing){
+            $0.colorMultiply(.accentColor)
+          }
+        }
+        
       }
     }
-    
+    /* .alert(isPresented: $showDialog,
+     TextAlert(title: "Enter friend name",message:"",text:self.friendName ?? "") { result in
+     if let text = result {
+     self.setFriend(text)
+     }
+     }) */
     .onAppear {
+      
+      let friends = NSUbiquitousKeyValueStore.default.object(forKey: CloudDefaultStorageKeys.friendsDict.rawValue) as? [String : String]
+      
+      switch(key().flatMap { friends?[$0] }) {
+      case .some(let name):
+        self.friendName = name
+        self.isFollowing = true
+      case .none:
+        self.isFollowing = false
+        self.friendName = .none
+      }
+      
       if let address = account.ethAddress {
+        
+        ENSContract.nameOfOwner(address, eth: web3.eth)
+          .done(on:.main) { $0.map { self.friendName = $0 } }
+          .catch { print($0) }
+        
         web3.eth.getBalance(address: address, block:.latest)
           .done(on:.main) { balance in
             self.balance = balance
           }.catch { print($0) }
+        
       }
+      
     }
   }
 }
