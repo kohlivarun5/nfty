3c6a2c9b676a70d901acc915439cf77bdc9bc2ce
Varun Kohli <kohli.varun5@gmail.com>
2022-05-31 00:19:22 -0400

    .

--- NFTY/Views/WalletTokensView.swift
+++ NFTY/Views/WalletTokensView.swift
@@ -22,5 +22,15 @@ struct WalletTokensView: View {
   
   var body: some View {
     WalletTokensSelector(tokens: tokens, enableNavLinks: true,selectedToken:$selectedToken)
+      .sheet(item: $selectedToken, onDismiss: { self.selectedToken = nil }) { token in
+        TokenTradeView(
+          nft: token.nft.nft,
+          price:.lazy(token.nft.indicativePrice),
+          collection:token.collection,
+          userWallet:userWallet,
+          isSheet:true)
+        .ignoresSafeArea(edges:.bottom)
+        .themeStyle()
+      }
   }
 }
