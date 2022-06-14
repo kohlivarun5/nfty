c80238d75e0f197f94e6ea3f044bb1d0d982ab47
Varun Kohli <kohli.varun5@gmail.com>
2022-06-13 21:32:43 -0400

    .

--- NFTY/Model/MakeErc721Collection.swift
+++ NFTY/Model/MakeErc721Collection.swift
@@ -36,6 +36,8 @@ struct MakeErc721Collection {
   
   static private let KnownUnsupportedName = "com.nftygo.unsupported"
   
+  private static var cache : [String:Erc721ContractInfo] = [:]
+  
   static private func validateAddress(_ addressStr:String) -> Promise<Erc721ContractInfo?> {
     let address = try! EthereumAddress(hex: addressStr, eip55: true)
     let ethContract = Erc721Contract.EthContract(address.hex(eip55: true))
@@ -47,20 +49,28 @@ struct MakeErc721Collection {
     .recover { e -> Promise<String?> in
       return Promise.value(nil)
     }
-    .map {
+    .map(on:.global(qos: .userInteractive)) {
       $0.map {
-        return Erc721ContractInfo(name:$0)
+        let ret = Erc721ContractInfo(name:$0)
+        MakeErc721Collection.cache[addressStr] = ret
+        return ret
       }
     }
   }
   
   static func ofAddress(address:EthereumAddress) -> Promise<Collection?> {
     
+    let addressStr = address.hex(eip55: true)
+    
+    if let info = cache[addressStr] {
+      return Promise.value(MakeErc721Collection.ofName(name:info.name,address: address))
+    }
+    
     let cache = CKJSONCache(
       database:CKContainer.default().publicCloudDatabase,
       bucket: "erc721Contracts",
       fallback:MakeErc721Collection.validateAddress)
-    return cache.get(address.hex(eip55: true))
+    return cache.get(addressStr)
       .map { (info:Erc721ContractInfo?) -> Collection? in
         switch (info?.name) {
         case .none: return nil
