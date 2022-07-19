f9ea0ecb57052e0233b3faddd198261204a3eb35
Varun Kohli <kohli.varun5@gmail.com>
2022-07-18 22:31:37 -0400

    .

--- NFTY/Model/ENSCached.swift
+++ NFTY/Model/ENSCached.swift
@@ -20,16 +20,15 @@ struct ENSCached {
   }
   
   static let avatarCache = HybridStorage(
-    memoryStorage: MemoryStorage<String, Avatar>(config: MemoryConfig(expiry: Expiry.seconds(3600))),
+    memoryStorage: MemoryStorage<String, Avatar>(config: MemoryConfig()),
     diskStorage: try! DiskStorage<String, Avatar>(
-      config: DiskConfig(name: "ENSCached.Avatar",expiry: Expiry.seconds(1200)),
+      config: DiskConfig(name: "ENSCached.Avatar"),
       transformer:TransformerFactory.forCodable(ofType:Avatar.self))
   )
   
   static public func avatarOwnerOfNamehash(_ nameHash:SolidityWrappedValue,block:BigUInt?,eth:Web3.Eth) -> Promise<(EthereumAddress?,String?)> {
     return Promise { seal in
       DispatchQueue.global(qos: .userInitiated).async {
-        try? avatarCache.removeExpiredObjects()
         switch(try? avatarCache.object(forKey:"\(nameHash.value.abiEncode(dynamic: false)!)@\(block ?? "")")) {
         case .some(let info):
           return seal.fulfill((info.owner,info.avatar))
