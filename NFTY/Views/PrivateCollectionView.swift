b9216f3a756d051f136f458cc98d1f12732a2347
Varun Kohli <kohli.varun5@gmail.com>
2022-08-12 23:48:43 -0400

    .

--- NFTY/Views/PrivateCollectionView.swift
+++ NFTY/Views/PrivateCollectionView.swift
@@ -11,11 +11,11 @@ import Web3
 struct PrivateCollectionView: View {
   
   enum TokensPage : Int {
-    case bought
-    case minted
-    case sales
     case owned
-    case for_sale
+    case sales
+    case minted
+    case saved
+    case following
   }
   @State private var tokensPage : TokensPage
   
@@ -25,6 +25,9 @@ struct PrivateCollectionView: View {
   let ensName : String?
   let isSheet : Bool
   
+  let friends : [String : String]
+  let addresses : [EthereumAddress]
+
   init(account:UserAccount,isOwnerView:Bool) {
     self.account = account
     self.isOwnerView = isOwnerView
@@ -32,6 +35,16 @@ struct PrivateCollectionView: View {
     self.ensName = nil
     self.isSheet = false
     _tokensPage = State(initialValue: .owned)
+    
+    if (isOwnerView) {
+      self.friends = NSUbiquitousKeyValueStore.default.object(forKey: CloudDefaultStorageKeys.friendsDict.rawValue) as? [String : String] ?? [:]
+      self.addresses = self.friends.compactMap { (key: String, value: String) in
+        try? EthereumAddress(hex: key, eip55: true)
+      }
+    } else {
+      self.friends = [:]
+      self.addresses = []
+    }
   }
   
   init(account:UserAccount,isOwnerView:Bool,avatar:(Collection,NFT)?,ensName:String?,page:TokensPage?,isSheet:Bool) {
@@ -41,6 +54,16 @@ struct PrivateCollectionView: View {
     self.ensName = ensName
     self.isSheet = isSheet
     _tokensPage = State(initialValue: page ?? .owned)
+    
+    if (isOwnerView) {
+      self.friends = NSUbiquitousKeyValueStore.default.object(forKey: CloudDefaultStorageKeys.friendsDict.rawValue) as? [String : String] ?? [:]
+      self.addresses = self.friends.compactMap { (key: String, value: String) in
+        try? EthereumAddress(hex: key, eip55: true)
+      }
+    } else {
+      self.friends = [:]
+      self.addresses = []
+    }
   }
   
   var body: some View {
@@ -54,33 +77,39 @@ struct PrivateCollectionView: View {
         addTopPadding:!isSheet,
         friendName: self.ensName,
         avatar:self.avatar)
-      
+           
       VStack(spacing:0) {
         
-        Picker(selection: Binding<Int>(
-          get: { self.tokensPage.rawValue },
-          set: { tag in
-            withAnimation { // needed explicit for transitions
-              self.tokensPage = TokensPage(rawValue: tag)!
+        Picker(
+          selection: Binding<Int>(
+            get: { self.tokensPage.rawValue },
+            set: { tag in
+              withAnimation { // needed explicit for transitions
+                self.tokensPage = TokensPage(rawValue: tag)!
+              }
+            }),
+          label: Text("")) {
+            Text("Owned").tag(TokensPage.owned.rawValue)
+            if (self.account.ethAddress != nil) { Text("Activity").tag(TokensPage.sales.rawValue) }
+            if (self.account.ethAddress != nil) { Text("Minted").tag(TokensPage.minted.rawValue) }
+            if (self.isOwnerView) {
+              Text("Saved").tag(TokensPage.saved.rawValue)
+              Text("Following").tag(TokensPage.following.rawValue)
             }
-          }),
-               label: Text("")) {
-          Text("Owned").tag(TokensPage.owned.rawValue)
-          if (self.account.ethAddress != nil) { Text("Activity").tag(TokensPage.sales.rawValue) }
-          if (self.account.ethAddress != nil) { Text("Minted").tag(TokensPage.minted.rawValue) }
-          // if (self.account.ethAddress != nil) { Text("Bought").tag(TokensPage.bought.rawValue) }
-          // Text("Sales").tag(TokensPage.sales.rawValue)
-        }
-               .pickerStyle(SegmentedPickerStyle())
-               .colorMultiply(.accentColor)
-               .padding([.trailing,.leading])
-               .padding(.top,5)
-               .padding(.bottom,7)
+            
+          }
+          .pickerStyle(SegmentedPickerStyle())
+          .colorMultiply(.accentColor)
+          .padding([.trailing,.leading])
+          .padding(.top,5)
+          .padding(.bottom,7)
         
         switch(self.tokensPage) {
-        case .bought:
+        case .owned:
+          WalletTokensView(tokens: getOwnerTokens(account),redactPrice:false)
+        case .sales:
           account.ethAddress.map {
-            FriendsFeedView(events:FriendsFeedViewModel(to:$0))
+            FriendsFeedView(events:FriendsFeedViewModel(from:$0,limit:2))
           }
         case .minted:
           account.ethAddress.map {
@@ -92,18 +121,12 @@ struct PrivateCollectionView: View {
                   limit:5)
               )
           }
-        case .sales:
-          account.ethAddress.map {
-            FriendsFeedView(events:FriendsFeedViewModel(from:$0,limit:2))
-          }
-        case .owned:
-          WalletTokensView(tokens: getOwnerTokens(account),redactPrice:false)
-        case .for_sale:
-          ActivityView(account:account,kind:.sales,emptyMessage:"No Active Sales")
+        case .saved:
+          FavoritesView()
+        case .following:
+          FriendsListView(friends:friends,addresses:addresses)
         }
-        
       }
-      
     }
     .navigationBarTitle("",displayMode:.large)
     .ignoresSafeArea(edges: .top)
