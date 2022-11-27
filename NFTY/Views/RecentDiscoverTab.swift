78dcd30ea2faeaeb72f8bdd9cee9a3dcea718179
Varun Kohli <vkohli@Varuns-MacBook-Air.local>
2022-11-27 13:21:54 -0500

    .

--- NFTY/Views/RecentDiscoverTab.swift
+++ NFTY/Views/RecentDiscoverTab.swift
@@ -1,26 +1,15 @@
-//
-//  RecentDiscoverTab.swift
-//  NFTY
-//
-//  Created by Varun Kohli on 8/6/22.
-//
+  //
+  //  RecentDiscoverTab.swift
+  //  NFTY
+  //
+  //  Created by Varun Kohli on 8/6/22.
+  //
 
 import SwiftUI
 
 struct RecentDiscoverTab: View {
   
   @ObservedObject var userWallet : UserWallet
-  
-  private let avatarView : ENSAvatarChangedFeedView
-  private let updatesView : ENSTextChangedFeedView
-  private let feedView : FeedView
-  
-  init(userWallet:UserWallet) {
-    self.userWallet = userWallet
-    self.avatarView = ENSAvatarChangedFeedView(events:ENSTextChangedViewModel(key: "avatar", limit: 5))
-    self.updatesView = ENSTextChangedFeedView(userWallet:userWallet,events:ENSTextChangedViewModel(key: "description", limit: 5))
-    self.feedView = FeedView(trades:CompositeCollection)
-  }
   
   enum Page : Int {
     case avatars
@@ -31,32 +20,45 @@ struct RecentDiscoverTab: View {
   @State private var page : Page = .updates
   
   var body: some View {
-    VStack(spacing:5) {
-      switch(page) {
-      case .avatars:
-        avatarView
-          .navigationBarTitle("Avatars",displayMode: .inline)
-      case .updates:
-        updatesView
-          .navigationBarTitle("Updates",displayMode: .inline)
-      case .recent:
-        feedView
-          .navigationBarTitle("Recent",displayMode: .inline)
-      }
+    
+    VStack(spacing:0) {
+      TabView(selection: Binding<Int>(
+        get: { self.page.rawValue },
+        set: { tag in
+          self.page = Page(rawValue: tag)!
+        })) {
+          ENSAvatarChangedFeedView(events:ENSTextChangedViewModel(key: "avatar", limit: 5))
+            .tag(Page.avatars.rawValue)
+            .navigationBarTitle("Avatars")
+          ENSTextChangedFeedView(userWallet:userWallet,events:ENSTextChangedViewModel(key: "description", limit: 5))
+            .tag(Page.updates.rawValue)
+            .navigationBarTitle("Updates")
+          FeedView(trades:CompositeCollection)
+            .tag(Page.recent.rawValue)
+            .navigationBarTitle("Recent")
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .navigationBarTitleDisplayMode(.inline)
+      
+      
       Picker(selection: Binding<Int>(
         get: { self.page.rawValue },
         set: { tag in
           self.page = Page(rawValue: tag)!
-        }),
-             label: Text("")) {
-        Text("Avatars").tag(Page.avatars.rawValue)
+        }
+      ), label: Text("")) {
+        Text("Avatars")
+          .tag(Page.avatars.rawValue)
         Text("Updates").tag(Page.updates.rawValue)
         Text("Recent").tag(Page.recent.rawValue)
       }
-             .pickerStyle(SegmentedPickerStyle())
-             .colorMultiply(.accentColor)
-             .padding([.trailing,.leading])
-             .padding(.bottom,7)
+      .pickerStyle(SegmentedPickerStyle())
+      .colorMultiply(.accentColor)
+      .padding([.trailing,.leading])
+      .padding(.bottom,5)
+      .padding(.top,10)
+      .background(.ultraThinMaterial)
     }
+    
   }
 }
